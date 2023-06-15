4ca3887a5d624216c365bce68b2f97c4eea100b7
Will Lowenkamp <lowenkamp97@gmail.com>
2023-06-15 09:46:06 -0400

    page displays hogs' names and images

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Nav from "./Nav";
-
 import hogs from "../porkers_data";
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<div className="hog-cards">
+				{hogs.map((hog) => (
+					<div key={hog.name} className="hog-card">
+						<img src={hog.image} alt={hog.name} />
+						<p>{hog.name}</p>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
+
 
 export default App;
